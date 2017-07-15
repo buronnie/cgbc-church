ef799d0b6fef2f1aab16d0ebe4cb282e6bf607f5
kaiguo <kai.guo@mycase.com>
2017-07-14 23:22:28 -0700

    update database config

--- react_ui/src/components/header/index.js
+++ react_ui/src/components/header/index.js
@@ -176,7 +176,7 @@ class Header extends Component {
     return (
     	<div>
 				<Row>
-					<Col span={8}><h5>Chinese Grace Bible Church</h5></Col>
+					<Col span={5} className="mt-2 ml-3"><h5>Chinese Grace Bible Church</h5></Col>
 					<Col span={16}>
 						<Menu
 							onClick={this.toggleNavItem}
