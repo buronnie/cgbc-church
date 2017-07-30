634086091910c0559eef73d3c399335a3580bebf
kaiguo <kai.guo@mycase.com>
2017-07-29 23:16:26 -0700

    setup rolify

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,6 +5,8 @@ class Offer < ApplicationRecord
   validates :offer_type, presence: true
   validate  :offered_at_must_be_a_valid_date
 
+  resourcify
+
   def offered_at_must_be_a_valid_date
     if !offered_at.is_a?(ActiveSupport::TimeWithZone)
       errors.add(:offered_at, 'is not a valid date')
