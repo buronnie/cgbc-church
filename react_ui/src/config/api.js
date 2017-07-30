8ae1d7daa7802dc87127436dd6d5054d36cce210
Kai Guo <buronnie@gmail.com>
2017-07-29 22:01:25 -0700

    Adaptive api url (#6)
    * test node env
    
    * adaptive select dev or prod api url
    
    * add http for localhost

--- react_ui/src/config/api.js
+++ react_ui/src/config/api.js
@@ -1,1 +1,8 @@
-export const apiUrl = 'https://obscure-brushlands-98802.herokuapp.com';
+let url;
+
+if (process.env.NODE_ENV === "production") {
+  url = 'https://obscure-brushlands-98802.herokuapp.com';
+} else {
+  url = 'http://localhost:3000';
+}
+export const apiUrl = url;
