e14b4625de6e1bb6e61facea97124b879b56e732
kaiguo <kai.guo@mycase.com>
2017-07-30 23:48:44 -0700

    use HOC to refactor offer forms

---
+++ react_ui/src/components/HOC/handleExceptionContainer.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { Spin } from 'antd';
+import My401Page from '../../components/exceptions/My401Page';
+import My404Page from '../../components/exceptions/My404Page';
+
+const handleExceptionContainer = (MyComponent, loadData) => {
+  return class extends Component {
+    constructor() {
+      super();
+      this.state = {
+        status: 'loading',
+      }
+    }
+
+    componentDidMount() {
+      loadData(this);
+    }
+
+    render() {
+      switch(this.state.status) {
+        case 'loading':
+          return <Spin className="loading-spin" />;
+        case 'success':
+          return (
+            <MyComponent
+              {...this.props}
+              {...this.state}
+            />
+          );
+        case 'Unauthorized':
+          return <My401Page />;
+        default:
+          return <My404Page />;
+      }
+    }
+  }
+};
+export default handleExceptionContainer;
--- react_ui/src/routes/finance/NewOffer.js
+++ react_ui/src/routes/finance/NewOffer.js
@@ -1,65 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import OfferForm from '../../components/finance/OfferForm';
 import offerFactory from '../../factories/offer';
+import handleExceptionContainer from '../../components/HOC/handleExceptionContainer';
 import moment from 'moment';
-import { Spin } from 'antd';
 import { Redirect } from 'react-router-dom';
-import My401Page from '../../components/exceptions/My401Page';
-import My404Page from '../../components/exceptions/My404Page';
 
 const redirectToUrl = (
-  <Redirect to="/" />
+  <Redirect to="/finance/offers" />
 );
 
-export default class NewOffer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: 'loading',
-    };
-  }
-
-  componentDidMount() {
-    offerFactory.newItem()
-      .then(res => {
-        this.setState({
-          status: 'success',
-          contributor: res.contributor,
-          amount: String(res.amount),
-          offer_type: res.offer_type,
-          offered_at: moment(res.offered_at),
-          note: res.note || '',
-        });
-      })
-      .catch(res => {
-        this.setState({
-          status: res.statusText,
-        });
+const loadData = (classRef) => {
+  offerFactory.newItem()
+    .then(res => {
+      classRef.setState({
+        status: 'success',
+        contributor: res.contributor,
+        amount: String(res.amount),
+        offer_type: res.offer_type,
+        offered_at: moment(res.offered_at),
+        note: res.note || '',
       });
-  }
+    })
+    .catch(res => {
+      classRef.setState({
+        status: res.statusText,
+      });
+    });
+};
 
+const NewOffer = (props) => (
+  <OfferForm
+    {...props}
+    action="new"
+    redirectToUrl={redirectToUrl}
+  />
+);
 
-  render() {
-    if (this.state.status === 'success') {
-      return (
-        <OfferForm
-          action="new"
-          {...this.state}
-          redirectToUrl={redirectToUrl}
-        />
-      );
-    } else if (this.state.status === 'Unauthorized') {
-      return (
-        <My401Page />
-      );
-    } else if (this.state.status === 'error') {
-      return (
-        <My404Page />
-      );
-    }
-
-    return (
-      <Spin className="loading-spin" />
-    );
-  }
-}
+export default handleExceptionContainer(
+  NewOffer,
+  loadData
+);
