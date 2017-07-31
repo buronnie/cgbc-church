f6c35116143643f510ea9e7a442ee834ec561d17
Kai Guo <buronnie@gmail.com>
2017-07-30 23:49:18 -0700

    List offers view (#9)
    * add table for offer list
    
    * use HOC to refactor offer forms

--- react_ui/src/routes/finance/EditOffer.js
+++ react_ui/src/routes/finance/EditOffer.js
@@ -1,66 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import OfferForm from '../../components/finance/OfferForm';
 import offerFactory from '../../factories/offer';
 import moment from 'moment';
-import { Spin } from 'antd';
 import { Redirect } from 'react-router-dom';
-import My401Page from '../../components/exceptions/My401Page';
-import My404Page from '../../components/exceptions/My404Page';
+import handleExceptionContainer from '../../components/HOC/handleExceptionContainer';
 
 const redirectToUrl = (
-  <Redirect to="/finance/offers/new" />
+  <Redirect to="/finance/offers" />
 );
 
-export default class EditOffer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: 'loading',
-    };
-  }
-
-  componentDidMount() {
-    const offer_id = this.props.match.params.id;
-    offerFactory.show(offer_id)
-      .then(res => {
-        this.setState({
-          status: 'success',
-          id: offer_id,
-          contributor: res.contributor,
-          amount: String(res.amount),
-          offer_type: res.offer_type,
-          offered_at: moment(res.offered_at),
-          note: res.note || '',
-        });
-      })
-      .catch(res => {
-        this.setState({
-          status: res.statusText,
-        });
+const loadData = (classRef) => {
+  const offer_id = classRef.props.match.params.id;
+  offerFactory.show(offer_id)
+    .then(res => {
+      classRef.setState({
+        status: 'success',
+        id: offer_id,
+        contributor: res.contributor,
+        amount: String(res.amount),
+        offer_type: res.offer_type,
+        offered_at: moment(res.offered_at),
+        note: res.note || '',
       });
-  }
+    })
+    .catch(res => {
+      classRef.setState({
+        status: res.statusText,
+      });
+    });
+};
 
-  render() {
-    if (this.state.status === 'success') {
-      return (
-        <OfferForm
-          action="edit"
-          {...this.state}
-          redirectToUrl={redirectToUrl}
-        />
-      );
-    } else if (this.state.status === 'Unauthorized') {
-      return (
-        <My401Page />
-      );
-    } else if (this.state.status === 'error') {
-      return (
-        <My404Page />
-      );
-    }
+const EditOffer = (props) => (
+  <OfferForm
+    {...props}
+    action="edit"
+    redirectToUrl={redirectToUrl}
+  />
+);
 
-    return (
-      <Spin className="loading-spin" />
-    );
-  }
-}
+export default handleExceptionContainer(
+  EditOffer,
+  loadData
+);
