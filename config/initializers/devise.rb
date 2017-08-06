4c78e2ba02760b3377d6fc4d41e0a782b14950b8
kaiguo <kai.guo@mycase.com>
2017-08-05 22:59:55 -0700

    remove devise key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,5 +2,4 @@ Devise.setup do |config|
   # Using rails-api, tell devise to not use ActionDispatch::Flash
   # middleware b/c rails-api does not include it.
   config.navigational_formats = [:json]
-  config.secret_key = ENV[‘DEVISE_SECRET_KEY’]
 end
