88e6e734534d38adcbdef60150773196c72f1449
kaiguo <kai.guo@mycase.com>
2017-08-06 00:03:32 -0700

    change api url in prod

--- react_ui/src/config/api.js
+++ react_ui/src/config/api.js
@@ -1,7 +1,7 @@
 let url;
 
 if (process.env.NODE_ENV === "production") {
-  url = 'https://obscure-brushlands-98802.herokuapp.com';
+  url = 'http://cgbc-env.93zkisfwvt.us-west-2.elasticbeanstalk.com/';
 } else {
   url = 'http://localhost:3000';
 }
